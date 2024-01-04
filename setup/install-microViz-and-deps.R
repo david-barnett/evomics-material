0b96de4dbfd22ff35dde942ecefdd9fb8b58cb14
David Barnett <david.barnett@maastrichtuniversity.nl>
2024-01-04 13:39:07 +0100

    update setup script following removal of corncob from CRAN

--- setup/install-microViz-and-deps.R
+++ setup/install-microViz-and-deps.R
@@ -11,14 +11,16 @@ install.packages(
   repos = c(davidbarnett = "https://david-barnett.r-universe.dev", getOption("repos"))
 )
 
-# Install suggested dependencies
+# Install suggested dependencies - necessary for workshop
 install.packages("ggraph") # for taxatree_plots()
 install.packages("DT") # for tax_fix_interactive()
-install.packages("corncob") # for example datasets and beta binomial models
 install.packages("ggtext") # for rotated text on ord_plot PCA figures
 install.packages("GUniFrac") # for (generalised) unifrac distances
 
-# just to be safe
+# more useful stuff just to be safe
 install.packages("tidyverse")
 install.packages("patchwork")
-install.packages("remotes") # for emergency github installs
+install.packages("remotes") 
+
+# install corncob from github for other example datasets and beta binomial models
+remotes::install_github('statdivlab/corncob@v0.4.1')
